217451fa317bf961b799b4b476896b265891cca2
lai-chun-lin <axd5201211@gmail.com>
2019-11-05 23:42:51 +0800

    使用save方法 來更改id為1的資料

--- routes/web.php
+++ routes/web.php
@@ -34,8 +34,13 @@ Route::get('/', function () {
 // $posts = \App\Post::where('id', '<' , 10)->orderBy('id', 'DESC')->get();
 // dd($posts);
 
+// $post = \App\Post::find(1);
+// $post->update([
+// 'title' => 'updated title',
+// 'content' => 'updated content',
+// ]);
+
 $post = \App\Post::find(1);
-$post->update([
-'title' => 'updated title',
-'content' => 'updated content',
-]);
+$post->title = 'saved title';
+$post->content = 'saved content';
+$post->save();
