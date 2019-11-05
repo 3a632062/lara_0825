41e35fd69f35545fbe1f4518df85c7308ed11cea
lai-chun-lin <axd5201211@gmail.com>
2019-11-05 23:33:09 +0800

    使用new來新增資料

--- routes/web.php
+++ routes/web.php
@@ -14,8 +14,13 @@
 Route::get('/', function () {
     return view('index');
 });
-\App\Post::create([
-'title' => 'test title',
-'content' => 'test content',
-]);
+// \App\Post::create([
+// 'title' => 'test title',
+// 'content' => 'test content',
+// ]);
 
+
+$post =new \App\Post();
+$post->title='test title';
+$post->content='test content';
+$post->save();
